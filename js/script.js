a83994cbff0a378947e5dfd58578eff30492cf38
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-12-13 14:40:25 +0000

    r2

--- js/script.js
+++ js/script.js
@@ -3,23 +3,18 @@
 */
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/Unit-6-02-JS/sw.js", {
+    scope: "/Unit-6-02-JS/",
   })
 }
 
-;("use strict")
+"use strict"
 
-function calculate() {
-  const length = parseInt(document.getElementById("pay").value)
-  const width = parseInt(document.getElementById("pay2").value)
+function myButtonClicked() {
+  if (localStorage.CookieCountGoUp) {
+    localStorage.CookieCountGoUp = Number(localStorage.CookieCountGoUp) + 1;
 
-  const area = length * width * 0.18
-  const perimeter = length * width * (1.0 - 0.18)
-  const TAX_RATE = 0.18
-
-  document.getElementById("area").innerHTML =
-    "The governemnt will take: " + area + "$"
-  document.getElementById("perimeter").innerHTML =
-    "your pay will be: " + perimeter + "$"
+  }
+  document.getElementById("answers").innerHTML =
+    "You Clicked This Cookie " + localStorage.CookieCountGoUp + " Times already";
 }
